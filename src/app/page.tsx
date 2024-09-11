926bad9d6e34dd3979cdbee2bf1637a2185bd242
Zohaib Shahzad <zohaibshahzad16@gmail.com>
2024-09-11 12:15:18 +0200

    Add Hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-   <div>
-    <h1 className="text-2xl text-center pt-2"> Zohaib</h1>
-   </div>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      {/* <h1 className="text-2xl text-center pt-2"> Zohaib</h1> */}
+      <Hero />
+    </main>
   );
 }
